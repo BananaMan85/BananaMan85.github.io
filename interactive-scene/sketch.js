1465b4b6be7e9ad2299117c3a60d415650118657
BananaMan85 <wsherwoodsk@gmail.com>
2025-02-26 13:20:31 -0600

    Imported interactive scene

---
+++ interactive-scene/sketch.js
@@ -0,0 +1,153 @@
+// Interactive Scene
+// William Sherwood
+// March 3, 2025
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let theColors = [
+  "white",
+  "white",
+  "black",
+  "black",
+  "blue",
+  "blue",
+  "red",
+  "red",
+  "yellow",
+  "yellow",
+];
+let x;
+let y;
+let size;
+let accuracy = 50;
+let growAccuracy = "grow";
+let accuracySpeed = 3;
+let keyJustPressed = false;
+let aimX;
+let aimY;
+let speedX;
+let speedY;
+let inertia = 1/50;
+let noiseOffsetX = 0;
+let noiseOffsetY = 1000; // Seperate noiseX from noiseY
+let noiseIncrement = 0.03;
+let driftX = 0;
+let driftY = 0;
+let wander = 1;
+
+let aimColor;
+
+let drawColoredTargetScene = true;
+let pickAccuracyScene = true;
+let drawAimScene = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  changeSize();
+  
+  aimColor = color(255,255,255);
+  aimColor.setAlpha(128);
+  
+  aimX = random(x-size/2, x+size/2)
+  aimY = random(y-size/2, y+size/2)
+}
+
+function draw() {
+  background(240);
+  runScenes();
+  
+  keyJustPressed = false;
+}
+
+
+function runScenes(){
+  if (drawColoredTargetScene){
+    drawColoredTarget();
+  }
+  if (pickAccuracyScene){
+    pickAccuracy();
+  }
+  if (drawAimScene){
+    drawAim();
+  }
+}
+
+function drawColoredTarget() {
+  for (let i = 0; i < theColors.length; i++){
+    fill(theColors[i]);
+    circle(x, y, size - i*(size/10))
+  }
+}
+
+function pickAccuracy(){  
+  fill(aimColor);
+  if (growAccuracy !== "stop"){
+    circle(x, y, accuracy);
+    if (growAccuracy === "grow"){
+      accuracy += accuracySpeed;
+      if (accuracy >= size){
+        growAccuracy = "shrink";
+      }
+    }
+    else if (growAccuracy === "shrink"){
+      accuracy -= accuracySpeed;
+      if (accuracy <= 0){
+        growAccuracy = "grow";
+      }
+    }
+
+    if (keyJustPressed){
+      growAccuracy = "stop";
+      drawAimScene = true;
+    }
+  }
+}
+
+function drawAim(){
+  fill(aimColor);
+  circle(aimX, aimY, accuracy)
+  
+  speedX = (mouseX - aimX) * inertia;
+  speedY = (mouseY - aimY) * inertia;
+  
+  if (dist(mouseX, mouseY, aimX, aimY) < accuracy){
+  
+    driftX = map(noise(noiseOffsetX), 0, 1, -wander, wander);
+    driftY = map(noise(noiseOffsetY), 0, 1, -wander, wander);
+
+    noiseOffsetX += noiseIncrement;
+    noiseOffsetY += noiseIncrement;
+  }
+  
+  aimX += speedX + driftX;
+  aimY += speedY + driftY;
+  
+  // console.log('X: ' + driftX);
+  // console.log('Y: ' + driftY);
+  // console.log(dist(mouseX, mouseY, aimX, aimY) < accuracy)
+}
+
+
+function keyPressed(){
+  if (!keyJustPressed){
+    keyJustPressed = true;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  changeSize();
+}
+
+function changeSize(){
+  x = width / 2;
+  y = height / 2;
+  if (width <= height){
+    size = width*(2/3);
+  }
+  else{
+    size = height*(2/3);
+  }
+}
